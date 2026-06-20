35415b36cf371b9af7311fbad3bfe94af96ccdc0
rohitrm16 <rohitmahato10@gmail.com>
2026-06-20 18:52:36 +0530

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const db = createClient(
-  'YOUR_SUPABASE_URL',
-  'YOUR_SUPABASE_ANON_KEY'
+  'https://etndkfbegeccwjupmzyq.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV0bmRrZmJlZ2VjY3dqdXBtenlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwOTEzNjYsImV4cCI6MjA5NDY2NzM2Nn0.JzuoJ4CY_DECkRVfFM5cL60Oe26P_NuUNLpRTiTJwOY'
 )
